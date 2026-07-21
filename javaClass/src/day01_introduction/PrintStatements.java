c400cd6d9b1bd9f3714f93f39770829364852053
Mahalakshmi <maha.sahasraa@gmail.com>
2026-07-21 22:09:46 +0530

    Day 1 Introduction

---
+++ javaClass/src/day01_introduction/PrintStatements.java
@@ -0,0 +1,29 @@
+package day01_introduction;
+
+public class PrintStatements {
+	
+    public static void main(String[] args) {
+
+        System.out.println("Java");
+        System.out.println("Program");
+        System.out.println("OOPs");
+
+        System.out.println(343);
+
+        System.out.println(34.3);
+
+        System.out.println('C' + 'C');
+
+        System.out.println(true);
+
+        System.out.println(34 + 3);
+
+        System.out.println("Java" + "Program");
+
+        System.out.println(1 + 2 + "Java" + "Program");
+
+        System.out.println(1 + 2 + "Java" + "Program" + 1 + 3);
+
+    }
+
+}
